118c01a6eaf3be1fc99ba059446c41abca6dcb8d
Jhon Steeve Dimanche <johnsteeve2002@gmail.com>
2023-03-02 14:13:24 -0500

    Carshop initial commit

---
+++ src/test/java/com/dfamily/demo/carShop/CarShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dfamily.demo.carShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
